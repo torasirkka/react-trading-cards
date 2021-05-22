1cff8548c8c01f99dff13036220d2c5485b08190
Tora Sirkka <tora@torasirkka.fi>
2021-05-21 21:05:23 -0700

    CChanged logicc from back end to front end.
    Data from backend is stored as a list of JS objects. This list is transformed into a list of react objects that finally are bundled together with React.Fragment. Together they make the parent react object that finally is rendered.

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -61,10 +61,24 @@ function TradingCard(props) {
   );
 }
 
-function TradingCardContainer(){
-  return(
-    <div>Hello World!</div>
+function TradingCardContainer() {
+  const tradingCards = [];
+
+  for (const currentCard of tradingCardData) {
+    tradingCards.push(
+      <TradingCard
+        name={currentCard.name}
+        skill={currentCard.skill}
+        imgUrl={currentCard.imgUrl}
+      />
+    );
+  }
+
+  return (
+    <React.Fragment>
+      {tradingCards}
+    </React.Fragment>
   );
 }
 
-ReactDOM.render(<TradingCardContainer/>, document.querySelector('#seedpy'))
+ReactDOM.render(<TradingCardContainer/>, document.querySelector('#container'))
